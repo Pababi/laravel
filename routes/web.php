8db175c99b20cfd66ad84283562588627c60821c
xraid <xraider@list.ru>
2025-12-06 17:24:23 +0500

    Пример использования POST

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,19 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
 //    return Inertia::render('welcome');
-    return 123;
-})->name('home');
+    $html = '<form method="post" action="create-post">
+<input type="text" value="Create Post">
+<button>Отправить</button>
+</form>';
+    return $html;
+
+});
+
+Route::get('/skills', function () {
+    return "тут будут мои функции123";
+});
+
+Route::post('/create-post', function () {
+    return "тут будут мои функции123";
+});
+
