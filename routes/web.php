932298dbdb6ada8d727fcec429b1c402b9791df3
xraid <xraider@list.ru>
2025-12-09 23:32:21 +0500

    Ограничение регулярного выражения

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,7 @@ Route::get('/posts/{post}/comments/{comment}', function (string $postId, string 
 });
 
 Route::get('/user/{id}', function (Request $request, string $id) {
-    return 'User '.$id;
+    return 'User ' . $id;
 });
 
 Route::get('/user/{name?}', function (?string $name = null) {
@@ -86,3 +86,39 @@ Route::get('/user/{name?}', function (?string $name = null) {
 Route::get('/user/{name?}', function (?string $name = 'John') {
     return $name;
 });
+
+Route::get('/user/{name}', function (string $name) {
+    // ...
+})->where('name', '[A-Za-z]+');
+
+Route::get('/user/{id}', function (string $id) {
+    // ...
+})->where('id', '[0-9]+');
+
+Route::get('/user/{id}/{name}', function (string $id, string $name) {
+    // ...
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
+Route::get('/user/{id}/{name}', function (string $id, string $name) {
+    // ...
+})->whereNumber('id')->whereAlpha('name');
+
+Route::get('/user/{name}', function (string $name) {
+    // ...
+})->whereAlphaNumeric('name');
+
+Route::get('/user/{id}', function (string $id) {
+    // ...
+})->whereUuid('id');
+
+Route::get('/user/{id}', function (string $id) {
+    // ...
+})->whereUlid('id');
+
+Route::get('/category/{category}', function (string $category) {
+    // ...
+})->whereIn('category', ['movie', 'song', 'painting']);
+
+Route::get('/category/{category}', function (string $category) {
+    return $category;
+})->whereIn('category', CategoryEnum::cases());
