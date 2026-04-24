718b39736baaec105d16af6b7cf5d7a281afdda9
xraid <xraider@list.ru>
2026-04-25 00:22:29 +0500

    Добавил расчёт квадратного уравнения

--- app/Http/Controllers/MathController.php
+++ app/Http/Controllers/MathController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 class MathController
 {
-    public function quadraticEquation() {
-        echo "Privet";
+    public function quadraticEquation(int $a, int $b, int $c) {
+        echo "a=" . $a . "<br>";
+        echo "b=" . $b . "<br>";
+        echo "c=" . $c . "<br>";
+        $d = $b * $b - 4 * $a * $c;
+        echo "D = " . $d . "<br>";
+        if ($d < 0 ){
+            echo "D меньше 0<br>";
+            echo "Действительных корней нет<br>";
+        } elseif ($d == 0 ){
+            echo "D равно 0<br>";
+            $x1 = -$b / 2 * $a;
+            echo "x1 = " . $x1 . "<br>";
+        }
+        else {
+            echo "D больше 0<br>";
+            $x1 = -$b + sqrt($d) / 2 * $a;
+            $x2 = -$b - sqrt($d) / 2 * $a;
+            echo "x1 = " . $x1 . "<br>";
+            echo "x2 = " . $x2 . "<br>";
+        }
+
     }
+
 }
