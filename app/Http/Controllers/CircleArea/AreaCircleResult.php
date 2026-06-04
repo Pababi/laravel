e89fe7239b6bc655f8eb111c6b027864ba1dbe70
xraid <xraider@list.ru>
2026-06-04 21:55:00 +0500

    Форматирование

--- app/Http/Controllers/CircleArea/AreaCircleResult.php
+++ app/Http/Controllers/CircleArea/AreaCircleResult.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\CircleArea;
 
 readonly class AreaCircleResult
 {
-public function __construct(
-    public float $pi,
-    public float   $area
-)
-{
+    public function __construct(
+        public float $pi,
+        public float $area
+    )
+    {
 
-}
+    }
 }
