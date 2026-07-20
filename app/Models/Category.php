e5964e5bcbc433a4b4869db56d1a51f58d55a0a0
xraid <xraider@list.ru>
2026-07-21 01:55:58 +0500

    Добавить именованный конструктор

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class category extends Model
+class Category extends Model
 {
+    public static function createCategory(string $title, string $topic,int $rating, string $description): self
+    {
+        $category = new self;
+        $category -> title = $title;
+        $category -> topic = $topic;
+        $category -> rating = $rating;
+        $category -> description = $description;
+        return $category;
+    }
 }
